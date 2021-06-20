8ed840d7ba04d15f0cd4ecc1b8702619f7115418
dbertozzi <david.a.bertozzi@gmail.com>
2021-06-20 18:27:31 -0400

    completed pair sum problem

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { getMaxProfit } from "./index";
+import { getMaxProfit, hasSumPair } from "./index";
 
 describe("getMaxProfit gets the max day trading profit from an array chronological stock prices", () => {
   test("it should get max profit", () => {
@@ -10,5 +10,23 @@ describe("getMaxProfit gets the max day trading profit from an array chronologic
     const stockPrices = [10, 7, 5, 4, 3, 2];
     const result = -1;
     expect(getMaxProfit(stockPrices)).toBe(result);
+  });
+});
+
+describe.only("hasSumPair return a boolean indicating whether or not two elements of numbers array can be added to equal sum", () => {
+  test("should return true", () => {
+    const inputArray = [0, 1, 2, 3, 4, 5];
+    const inputSum = 4;
+    expect(hasSumPair(inputSum, inputArray)).toBeTruthy();
+  });
+  test("should return false", () => {
+    const inputArray = [0, 1];
+    const inputSum = 4;
+    expect(hasSumPair(inputSum, inputArray)).toBeFalsy();
+  });
+  test("should return false because numbers can only be used once", () => {
+    const inputArray = [0, 2, 3];
+    const inputSum = 4;
+    expect(hasSumPair(inputSum, inputArray)).toBeFalsy();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,17 @@ export const getMaxProfit = (stockPrices: number[]) => {
   }
   return bestProfit;
 };
+
+export const hasSumPair = (sum: number, numbers: number[]): boolean => {
+  if (numbers.length < 2) {
+    throw new Error("must have at least 2 elements in numbers array");
+  }
+  const sumsHash = new Set();
+  for (let i = 0; i < numbers.length; i++) {
+    if (sumsHash.has(numbers[i])) {
+      return true;
+    }
+    sumsHash.add(sum - numbers[i]);
+  }
+  return false;
+};
