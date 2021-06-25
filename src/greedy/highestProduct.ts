57c3add02492712c035f00c441a4922959ead4a1
dbertozzi <david.a.bertozzi@gmail.com>
2021-06-25 17:14:22 -0400

    highest product finds kth product
    - configured highProduct to find the highest product of k integers

--- src/greedy/highestProduct.ts
+++ src/greedy/highestProduct.ts
@@ -2,13 +2,20 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  * The input arrayOfInts will always have at least three integers.
  */
-const highestProduct = (arrayOfInts: number[]): number => {
-    const NUMBER_OF_INTS_IN_PRODUCT = 3;
-    const biggestNegativeInts = new Array(NUMBER_OF_INTS_IN_PRODUCT).fill(0);
-    const biggestPositiveInts = new Array(NUMBER_OF_INTS_IN_PRODUCT).fill(0);
+const highestProduct = (
+    arrayOfInts: number[],
+    productCount: number = 3
+): number => {
+    if (arrayOfInts.length < productCount) {
+        throw new Error(
+            `Expected arrayOfInts to have length of ${productCount} or more.`
+        );
+    }
+    const biggestNegativeInts = new Array(productCount).fill(0);
+    const biggestPositiveInts = new Array(productCount).fill(0);
 
     arrayOfInts.forEach((number) => {
-        for (let i = 0; i < NUMBER_OF_INTS_IN_PRODUCT; i++) {
+        for (let i = 0; i < productCount; i++) {
             if (number > biggestPositiveInts[i]) {
                 biggestPositiveInts[i] = number;
                 break;
@@ -19,15 +26,30 @@ const highestProduct = (arrayOfInts: number[]): number => {
             }
         }
     });
-    const biggestNegativeProduct =
-        biggestNegativeInts.slice(0, 2).reduce((result, num) => result * num) *
-        biggestPositiveInts[0];
-    const biggestPositiveProduct = biggestPositiveInts.reduce(
-        (result, num) => result * num
-    );
-    return biggestNegativeProduct > biggestPositiveProduct
-        ? biggestNegativeProduct
-        : biggestPositiveProduct;
+    const biggestProductInts = [...biggestPositiveInts];
+    let negativeIntInsertIndex = biggestProductInts.length - 2;
+    for (let i = 0; i < biggestNegativeInts.length - 1; i++) {
+        if (biggestNegativeInts[i] === 0) {
+            break;
+        }
+        if (
+            biggestNegativeInts
+                .slice(i, i + 2)
+                .reduce((result, num) => result * num) >
+            biggestProductInts
+                .slice(negativeIntInsertIndex, negativeIntInsertIndex + 2)
+                .reduce((result, num) => result * num)
+        ) {
+            biggestProductInts[negativeIntInsertIndex] = biggestNegativeInts[i];
+            biggestProductInts[negativeIntInsertIndex + 1] =
+                biggestNegativeInts[i + 1];
+            negativeIntInsertIndex -= 2;
+            if (negativeIntInsertIndex < 0) {
+                break;
+            }
+        }
+    }
+    return biggestProductInts.reduce((result, num) => result * num);
 };
 
 export default highestProduct;
