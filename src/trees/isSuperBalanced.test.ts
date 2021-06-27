954ec02a8693543c4913957f2d418e0b02ab27fd
dbertozzi <david.a.bertozzi@gmail.com>
2021-06-27 14:03:49 -0400

    refactor isSuperBalanced w shared BinaryTree class
    - refactored this problem to clean it up
    - now uses shared BinaryTree class
    - removed recursive solution

--- src/trees/isSuperBalanced.test.ts
+++ src/trees/isSuperBalanced.test.ts
@@ -1,10 +1,11 @@
-import { BinaryTree, BinaryTreeNode } from "./isSuperBalanced";
+import { SuperBalancedBinaryTree } from "./isSuperBalanced";
+import { BinaryTreeNode } from "./BinaryTree";
 
 describe("should determine if tree has leaf nodes at depths differing by more than 1", () => {
     test("it should return true", () => {
         // level 1
         const zero = new BinaryTreeNode(0);
-        const tree = new BinaryTree(zero);
+        const tree = new SuperBalancedBinaryTree(zero);
         // level 2
         const two = new BinaryTreeNode(2);
         const one = new BinaryTreeNode(1);
@@ -15,12 +16,12 @@ describe("should determine if tree has leaf nodes at depths differing by more th
         const four = new BinaryTreeNode(4);
         one.left = three;
         one.right = four; // leaf depth 3
-        expect(tree.isSuperBalanced(true)).toBeTruthy();
+        expect(tree.isSuperBalanced()).toBeTruthy();
     });
     test("it should return true", () => {
         // level 1
         const zero = new BinaryTreeNode(0);
-        const tree = new BinaryTree(zero);
+        const tree = new SuperBalancedBinaryTree(zero);
         // level 2
         const two = new BinaryTreeNode(2);
         const one = new BinaryTreeNode(1);
@@ -32,6 +33,6 @@ describe("should determine if tree has leaf nodes at depths differing by more th
         // level 4
         const four = new BinaryTreeNode(4);
         three.left = four; // leaf depth 4
-        expect(tree.isSuperBalanced(true)).toBeFalsy();
+        expect(tree.isSuperBalanced()).toBeFalsy();
     });
 });
--- src/trees/isSuperBalanced.ts
+++ src/trees/isSuperBalanced.ts
@@ -1,43 +1,12 @@
-export class BinaryTreeNode {
-    value: number | string;
-    left: BinaryTreeNode | null;
-    right: BinaryTreeNode | null;
-    constructor(value: number | string = 0) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+import { BinaryTree, BinaryTreeNode } from "./BinaryTree";
 
-    insertLeft(value: number) {
-        this.left = new BinaryTreeNode(value);
-        return this.left;
-    }
-
-    insertRight(value: number) {
-        this.right = new BinaryTreeNode(value);
-        return this.right;
-    }
-}
-
-export class BinaryTree {
-    root: BinaryTreeNode;
+export class SuperBalancedBinaryTree extends BinaryTree {
     leafDepths: Set<number>;
     constructor(root: BinaryTreeNode) {
-        this.root = root;
+        super(root);
         this.leafDepths = new Set();
     }
-    preOrderTraverse(node: BinaryTreeNode, depth: number = 0) {
-        if (node.left !== null) {
-            this.preOrderTraverse(node.left, depth + 1);
-        }
-        if (node.right !== null) {
-            this.preOrderTraverse(node.right, depth + 1);
-        }
-        if (node.right === null && node.left === null) {
-            this.leafDepths.add(depth);
-        }
-    }
-    preOrderStack() {
+    isSuperBalanced() {
         const nodes: Array<[BinaryTreeNode, number]> = [[this.root, 1]];
         while (nodes.length > 0) {
             const [node, depth] = nodes.pop() as [BinaryTreeNode, number];
@@ -59,20 +28,5 @@ export class BinaryTree {
             }
         }
         return true;
-    }
-    isSuperBalanced(useStack: boolean = false) {
-        if (useStack) {
-            return this.preOrderStack();
-        } else {
-            this.preOrderTraverse(this.root, 1);
-            if (this.leafDepths.size === 2) {
-                const depths = [...this.leafDepths].sort();
-                if (depths[depths.length - 1] - depths[0] > 1) {
-                    return false;
-                }
-            }
-            if (this.leafDepths.size > 2) return false;
-            return true;
-        }
     }
 }
